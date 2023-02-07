91c8e303a5e3374bef572e7b26728fd4102a9332
cx9s <cx9918@gmail.com>
2023-02-07 13:44:59 +0000

    set default image, and remove '/revision...'

--- src/components/character.tsx
+++ src/components/character.tsx
@@ -1,21 +1,27 @@
-import { DisneyCharacter } from "../disney_character"
+import { DisneyCharacter } from "../disney_character";
 
-// for our props we can reuse the DisneyCharacter interface
-// - defining an anonymous type that just has one property - a DisneyCharacter
-const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) => 
-  
+const Character: React.FC<{ character: DisneyCharacter }> = ({ character }) => {
+  let imageSrc = character.imageUrl
+    ? character.imageUrl
+    : "https://picsum.photos/300/200/?blur";
+
+  if (imageSrc.indexOf("/revision") !== -1) {
+    imageSrc = imageSrc.substring(0, imageSrc.indexOf("/revision"));
+  }
+
+  return (
     <article className="character-item">
-
       <h2>{character.name}</h2>
 
-      <div className="character-item__actions">
-        Add to Favourites
-      </div>
+      <div className="character-item__actions">Add to Favourites</div>
 
-      <img className="character-item__img" src={character.imageUrl} alt={character.name} />
-
+      <img
+        className="character-item__img"
+        src={imageSrc}
+        alt={character.name}
+      />
     </article>
-  
+  );
+};
 
-
-export default Character
+export default Character;
