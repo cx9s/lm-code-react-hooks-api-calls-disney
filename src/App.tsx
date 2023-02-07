db017ff839e5696a840a2a514ef1146c6b70d48f
cx9s <cx9918@gmail.com>
2023-02-07 12:22:02 +0000

    useEffect for fetch() data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Header from "./components/header";
+import CharacterContainer from "./components/character_container";
+import Navigation from "./components/navigation";
+import { DisneyCharacter } from "./disney_character";
 
-import './App.css';
-import React, { useState } from 'react';
-import Header from './components/header';
-import CharacterContainer from './components/character_container';
-import Navigation from './components/navigation';
-import { DisneyCharacter } from './disney_character';
+const App: React.FC = () => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([]);
 
-const App : React.FC = () => {
+  const getCharacters = async (pageNumber: number) => {
+    const apiResponse = await fetch(
+      `http://api.disneyapi.dev/characters?page=${pageNumber}`
+    );
+    const json = (await apiResponse.json()) as { data: DisneyCharacter[] };
+    setCharacters(json.data);
+  };
 
-	const [currentPage, setCurrentPage] = useState<number>(1);
-
-  // Some dummy state representing disney characters
-  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([
-    {
-      _id: 6,
-      name: "'Olu Mel",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/6/61/Olu_main.png"
-    },
-    {
-      _id: 25,
-      name: "Abu",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/3/3f/Profile_-_Abu.png"
-    },
-    {
-      _id: 30,
-      name: "Ace",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/1/1e/Profile_-_Ace.png"
-    },
-  ]);
-
-  
+  useEffect(() => {
+    getCharacters(1);
+  }, []);
 
   return (
     <div className="page">
@@ -38,6 +28,6 @@ const App : React.FC = () => {
       <CharacterContainer characters={characters} />
     </div>
   );
-}
+};
 
 export default App;
